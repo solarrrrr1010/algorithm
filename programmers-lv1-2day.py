073a12c9e6eb1bbf97c0877116174b0a588f7753
kimbongcheol <solarrrrr1010@gmail.com>
2022-01-09 01:41:16 +0900

    Modify: lv1. 카카오 조건에 맞는 아이디 생성하기 문제 - 완료

--- programmers-lv1-2day.py
+++ programmers-lv1-2day.py
@@ -1,8 +1,17 @@
 import re
 
 def solution(new_id):
-  new_id = new_id.lower()
-  new_id = re.sub('\.\.',".", new_id)
-  new_id = re.sub('[^a-z0-9\-\_\.]|^[\.]|[\.]$',"", new_id)
-  answer = re.sub('^$',"a", new_id)
+  answer = new_id.lower() 
+  answer = re.sub('[^a-z0-9\-\_\.]',"", answer) 
+  answer = re.sub('\.+',".", answer) 
+  answer = re.sub('^[.]|[.]$', "", answer)  
+  answer = re.sub('^$',"a", answer)
+  answer = answer[:15] 
+  answer = re.sub('^[.]|[.]$', "", answer) 
+
+  if len(answer) < 3:
+    add_str = answer[-1]
+    while 3-len(answer):
+      answer = answer + add_str
+    
   return answer
